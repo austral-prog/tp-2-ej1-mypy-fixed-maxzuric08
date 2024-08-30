0f2fb0989e4b086cdb92f8d5747e6141af51dbf7
Matias Ponce <matiaseponce@gmail.com>
2024-08-30 11:19:35 -0300

    Update maximums.py

--- src/maximums.py
+++ src/maximums.py
@@ -1,14 +1,14 @@
 def max_of_two(x: int, y: int) -> int:
-    if x>=y:
+    if x >= y:
         return x
-    elif x<=y:
+    elif x <= y:
         return y
 
 
-def max_of_three(x:int, y:int, z:int) -> int:
-    if y<=z and x<=z:
+def max_of_three(x: int, y: int, z: int) -> int:
+    if y <= z and x <= z:
         return z
-    elif z<=x and y<=x:
+    elif z <= x and y <= x:
         return x
-    elif x<=y and z<=y:
+    elif x <= y and z <= y:
         return y
